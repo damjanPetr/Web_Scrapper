729c4e00b170c3f20e059d1251468fdde66fc7f0
damjanPetr <pedamovski@gmail.com>
2024-02-25 23:20:31 +0100

    test: add json a testing

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,33 +1,53 @@
-import puppeteer from "puppeteer";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
+import puppeteer, { Browser, Page } from "puppeteer";
 
-import {
-  beforeEach,
-  afterEach,
-  it,
-  expect,
-  describe,
-  vi,
-  beforeAll,
-  afterAll,
-} from "vitest";
+describe("add page should produce result", async () => {
+  let browser: Browser;
+  let page: Page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ headless: false });
+    page = await browser.newPage();
+  });
 
-const browser = await puppeteer.launch({
-  headless: "new",
-});
+  afterAll(async () => {
+    browser.close();
+  });
 
-const page = await browser.newPage();
-beforeAll(async () => {
-  await page.goto("http://localhost:3000");
-});
+  test("json web site", async () => {
+    await page.goto("http://localhost:3000/add", {
+      waitUntil: "load",
+    });
 
-describe("check if the homepage is loading properly", () => {
-  it("check if the homepage is loading properly", () => {
-    page.$("#search").then((input) => {
-      expect(input).toBeDefined();
+    const link = await page.waitForSelector('input[name="link"]');
+    const title = await page.waitForSelector('input[name="title"]');
+
+    const selectorName = await page.waitForSelector(
+      'input[name="selectorName"]',
+    );
+    const selector = await page.waitForSelector('input[name="page$$"]');
+
+    const formBtn = await page.waitForSelector('form button[type="submit"]');
+
+    await title?.type("test");
+    await link?.type("https://jsonplaceholder.typicode.com/");
+
+    await selectorName?.type("test1");
+    await selector?.type("a");
+
+    await formBtn?.click();
+    const result = await page.waitForSelector("#results");
+    await page.waitForSelector("#results > div");
+
+    console.log(await result?.evaluate((e: any) => e.dataset["res"]));
+    /* to have a result */
+    const data = await result?.evaluate((e: any) => {
+      return e.dataset["res"];
+    });
+    expect(data).toBe("true");
+
+    await page.screenshot({
+      path: "./testData/screenshot.png",
+      fullPage: true,
     });
   });
-});
-
-afterAll(async () => {
-  browser.close();
 });
